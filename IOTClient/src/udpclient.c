37a645a76b506b9e3ab365186490a49b90fdfa97
David Andrino <davidandrino2002@gmail.com>
2023-04-27 05:44:40 -0400

    Select ip and port in client. Fixed test message

--- IOTClient/src/udpclient.c
+++ IOTClient/src/udpclient.c
@@ -3,10 +3,14 @@
  * @author David Andrino & Fernando Sanz
  */
 #include <arpa/inet.h>
-#include <stdio.h>
-#include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DEBUG
+#define MAX_IP_LENGTH 32
 
 int main(void) {
     int                socket_desc;
@@ -27,14 +31,31 @@ int main(void) {
     }
     printf("Socket created successfully\n");
 
+    int  port;
+    char ip[MAX_IP_LENGTH];
+    fflush(stdin);
+    printf("Enter server ip: ");
+    if (!scanf("%31[^\n]", ip)) return 1;
+    fflush(stdin);
+    printf("Enter server port: ");
+    if (!scanf("%d", &port)) return 1;
+    fflush(stdin);
+#ifdef DEBUG
+    printf("[DEBUG] Connecting to: %s:%d\n", ip, port);
+#endif
+
     // Set port and IP:
     server_addr.sin_family      = AF_INET;
-    server_addr.sin_port        = htons(2000);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_port        = htons(port);
+    server_addr.sin_addr.s_addr = inet_addr(ip);
 
+#ifdef DEBUG
+    printf("[DEBUG] Created struct successfully\n");
+#endif
     // Get input from the user:
-    printf("Enter message: ");
-    fgets(client_message, sizeof(client_message), stdin);
+    // printf("Enter message: ");
+    // if (!scanf("%1999[^\n]", client_message)) return 1;
+    strcpy(client_message, "TEST MESSAGE");
 
     // Send the message to server:
     if (sendto(socket_desc, client_message, strlen(client_message), 0, (struct sockaddr *)&server_addr, server_struct_length) < 0) {
