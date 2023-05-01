261f0668af816a71c02a7f68cd7a3b0671a34ada
David Andrino <davidandrino2002@gmail.com>
2023-05-01 05:56:06 -0400

    Create rpiclient.c

--- RaspberryClient/src/rpiclient.c
+++ RaspberryClient/src/rpiclient.c
@@ -1,78 +1,53 @@
 /**
- * @brief Basic C UDP client
+ * @brief Basic Raspberry Pi udp client. Connects to the server and sends the readings as JSON text
  * @author David Andrino & Fernando Sanz
  */
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <signal.h>
+#include <unistd.h>
 
-#define DEBUG
-#define MAX_IP_LENGTH 32
+#include "accelerometer/accelerometer.h"
+#include "colorSensor/colorSensor.h"
+#include "json/json.h"
+#include "udpclient/udpclient.h"
 
-int main(void) {
-    int                socket_desc;
-    struct sockaddr_in server_addr;
-    char               server_message[2000], client_message[2000];
-    unsigned int       server_struct_length = sizeof(server_addr);
+volatile int stop = 0;
 
-    // Clean buffers:
-    memset(server_message, '\0', sizeof(server_message));
-    memset(client_message, '\0', sizeof(client_message));
+void sigint_isr(int s) {
+    stop = 1;
+}
 
-    // Create socket:
-    socket_desc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-
-    if (socket_desc < 0) {
-        printf("Error while creating socket\n");
-        return -1;
-    }
-    printf("Socket created successfully\n");
-
-    int  port;
-    char ip[MAX_IP_LENGTH];
-    fflush(stdin);
-    printf("Enter server ip: ");
-    if (!scanf("%31[^\n]", ip)) return 1;
-    fflush(stdin);
-    printf("Enter server port: ");
-    if (!scanf("%d", &port)) return 1;
-    fflush(stdin);
-#ifdef DEBUG
-    printf("[DEBUG] Connecting to: %s:%d\n", ip, port);
-#endif
-
-    // Set port and IP:
-    server_addr.sin_family      = AF_INET;
-    server_addr.sin_port        = htons(port);
-    server_addr.sin_addr.s_addr = inet_addr(ip);
-
-#ifdef DEBUG
-    printf("[DEBUG] Created struct successfully\n");
-#endif
-    // Get input from the user:
-    // printf("Enter message: ");
-    // if (!scanf("%1999[^\n]", client_message)) return 1;
-    strcpy(client_message, "TEST MESSAGE");
-
-    // Send the message to server:
-    if (sendto(socket_desc, client_message, strlen(client_message), 0, (struct sockaddr *)&server_addr, server_struct_length) < 0) {
-        printf("Unable to send message\n");
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        printf("Usage: %s IP PORT", argv[0]);
         return -1;
     }
 
-    // Receive the server's response:
-    if (recvfrom(socket_desc, server_message, sizeof(server_message), 0, (struct sockaddr *)&server_addr, &server_struct_length) < 0) {
-        printf("Error while receiving server's msg\n");
-        return -1;
+    signal(SIGINT, sigint_isr);
+
+    // Initialize sensors and clients
+    acc_init();
+    cs_init();
+    udp_init(argv[1], atoi(argv[2]));
+
+    color_t color[10];
+    acc_t   acc[10];
+    while (!stop) {
+        for (int i = 0; i < 10; i++) {
+            // Read data 10 times, waiting 1 second
+            acc_read(&(acc[0]));
+            cs_read_clear_corrected(&(color[0]));
+            sleep(1);
+        }
+
+        // Send data to UDP server
+
+        // Sleep for
     }
 
-    printf("Server's response: %s\n", server_message);
-
-    // Close the socket:
-    close(socket_desc);
-
-    return 0;
+    // Close the file descriptors
+    acc_close();
+    cs_close();
+    udp_close();
 }
