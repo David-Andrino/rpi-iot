41217c3de27d7c4fa3a246b9d7295f85c004c381
fersanz123 <fersanzpersonal@gmail.com>
2023-05-01 17:40:51 +0200

    Changing the function to receive an array of accelerations and colors

--- RaspberryClient/src/json/json.c
+++ RaspberryClient/src/json/json.c
@@ -4,27 +4,36 @@
 #include "cJSON.h"
 #include "json.h"
 
-int generateJSON(char *str, uint8_t r, uint8_t g, uint8_t b, float x, float y, float z) {
-
-    cJSON *acc = cJSON_CreateObject();
-    cJSON_AddNumberToObject(acc, "x", x);
-    cJSON_AddNumberToObject(acc, "y", y);
-    cJSON_AddNumberToObject(acc, "z", z);
-
-    cJSON *color = cJSON_CreateObject();
-    cJSON_AddNumberToObject(color, "r", r);
-    cJSON_AddNumberToObject(color, "g", g);
-    cJSON_AddNumberToObject(color, "b", b);
+int json_generate(char *str, color_t *colors, acc_t *accs) {
 
     cJSON *acc_color = cJSON_CreateObject();
-    cJSON_AddItemToObject(acc_color, "Acceleration", acc);
-    cJSON_AddItemToObject(acc_color, "Color", color);
+    cJSON_AddItemToObject(acc_color, "Accelerations", cJSON_CreateArray());
+    cJSON_AddItemToObject(acc_color, "Colors", cJSON_CreateArray());
 
-    char *tmp = cJSON_Print(acc_color);
-    strcpy(str, tmp);
-    int len = strlen(str);
+    cJON *colors = cJSON_GetObjectItem(acc_color, "Colors");
+    cJON *accs = cJSON_GetObjectItem(acc_color, "Accelerations");
+
+    for (int i = 0; i < 10; i++){
+        cJSON *color = cJSON_CreateObject();
+        cJSON *acc = cJSON_CreateObject();
+
+        cJSON_AddNumberToObject(color, "r", colors[i].r);
+        cJSON_AddNumberToObject(color, "g", colors[i].g);
+        cJSON_AddNumberToObject(color, "b", colors[i].b);
+
+        cJSON_AddNumberToObject(acc, "x", accs[i].x);
+        cJSON_AddNumberToObject(acc, "y", accs[i].y);
+        cJSON_AddNumberToObject(acc, "z", accs[i].z);
+
+        cJSON_AddItemToArray(colors,color);
+        cJSON_AddItemToArray(accs,acc);
+    }
+
+    char *json_str = cJSON_PrintUnformatted(acc_color);
+    strcpy(str, json_str);
 
     cJSON_Delete(acc_color);
 
-    return len;
+
+    return strlen(str);
 }
