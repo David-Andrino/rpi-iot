f21395106fe9276e10f63b5feb8cbd33bfee6257
David Andrino <davidandrino2002@gmail.com>
2023-05-02 12:10:05 -0400

    Fix include and caps

--- RaspberryClient/src/json/json.c
+++ RaspberryClient/src/json/json.c
@@ -1,7 +1,7 @@
 #include <string.h>
 #include <stdint.h>
 
-#include "cJSON.h"
+#include <cJSON.h>
 #include "json.h"
 
 int json_generate(char *str, color_t *colors, acc_t *accs) {
@@ -11,19 +11,19 @@ int json_generate(char *str, color_t *colors, acc_t *accs) {
     for (int i = 0; i < 10; i++){
         cJSON *sample = cJSON_CreateObject();
 
-        cJSON_AddNumberToObject(sample, "Sample", i);
+        cJSON_AddNumberToObject(sample, "sample", i);
 
         cJSON *acc = cJSON_CreateObject();
         cJSON_AddNumberToObject(acc, "x", accs[i].x);
         cJSON_AddNumberToObject(acc, "y", accs[i].y);
         cJSON_AddNumberToObject(acc, "z", accs[i].z);
-        cJSON_AddItemToObject(sample, "Acceleration", acc);
+        cJSON_AddItemToObject(sample, "acceleration", acc);
 
         cJSON *color = cJSON_CreateObject();
         cJSON_AddNumberToObject(color, "r", colors[i].r);
         cJSON_AddNumberToObject(color, "g", colors[i].g);
         cJSON_AddNumberToObject(color, "b", colors[i].b);
-        cJSON_AddItemToObject(sample, "Color", color);
+        cJSON_AddItemToObject(sample, "color", color);
 
         cJSON_AddItemToArray(acc_color, sample);
 
