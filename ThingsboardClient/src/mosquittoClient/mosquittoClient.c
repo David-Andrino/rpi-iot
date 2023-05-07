6f63e599f97b495c4021ca535ca9dfa35266520b
David Andrino <davidandrino2002@gmail.com>
2023-05-07 11:09:13 -0400

    Separate credentials

--- ThingsboardClient/src/mosquittoClient/mosquittoClient.c
+++ ThingsboardClient/src/mosquittoClient/mosquittoClient.c
@@ -3,11 +3,19 @@
 #include <string.h>
 
 #include "mosquittoClient.h"
+#include "credentials.h"
 
 #define COMMAND_MAX_LENGTH 2000
-#define DEBUG
  
 void mqtt_send_data(char* data) {
+#ifndef HOSTNAME
+    printf("Missing credentials!");
+    exit(1);
+#endif
+#ifndef TOKEN
+    printf("Missing credentials!");
+    exit(1);
+#endif
     char* command = malloc(COMMAND_MAX_LENGTH * sizeof(char));
     sprintf(command, "mosquitto_pub -d -q 1 -h \"%s\" -p \"1883\" -t \"v1/devices/me/telemetry\" -u \"%s\" -m %s", HOSTNAME, TOKEN, data);
 #ifndef DEBUG
--- ThingsboardClient/src/mosquittoClient/mosquittoClient.h
+++ ThingsboardClient/src/mosquittoClient/mosquittoClient.h
@@ -1,9 +1,6 @@
 #ifndef MOSQUITTO_H 
 #define MOSQUITTO_H
 
-#define HOSTNAME "hostname"
-#define TOKEN    "token"
-
 extern void mqtt_send_data(char* data);
 
 #endif
