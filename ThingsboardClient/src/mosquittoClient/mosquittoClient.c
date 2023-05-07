a78fdd81e4b8dee97ca259d93edc20ee748f4567
David Andrino <davidandrino2002@gmail.com>
2023-05-07 07:26:14 -0400

    Implement mosquittoClient

--- ThingsboardClient/src/mosquittoClient/mosquittoClient.c
+++ ThingsboardClient/src/mosquittoClient/mosquittoClient.c
@@ -1,5 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "mosquittoClient.h"
 
+#define COMMAND_MAX_LENGTH 2000
+#define DEBUG
+ 
 void mqtt_send_data(char* data) {
-
+    char* command = malloc(COMMAND_MAX_LENGTH * sizeof(char));
+    sprintf(command, "mosquitto_pub -d -q 1 -h \"%s\" -p \"1883\" -t \"v1/devices/me/telemetry\" -u \"%s\" -m %s", HOSTNAME, TOKEN, data);
+#ifndef DEBUG
+    system(command);
+#else 
+    printf("[DEBUG] Command: %s\n", command);
+#endif
+    free(command);
 }
--- ThingsboardClient/src/mosquittoClient/mosquittoClient.h
+++ ThingsboardClient/src/mosquittoClient/mosquittoClient.h
@@ -1,8 +1,8 @@
 #ifndef MOSQUITTO_H 
 #define MOSQUITTO_H
 
-#define HOSTNAME hostname
-#define TOKEN    token
+#define HOSTNAME "hostname"
+#define TOKEN    "token"
 
 extern void mqtt_send_data(char* data);
 
